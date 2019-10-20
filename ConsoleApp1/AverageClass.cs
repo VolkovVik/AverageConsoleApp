80b5df24c5726f57d5ca981f6f518a028f9a127a
Волков Виктор <volkov.vik1@gmail.com>
2019-10-20 22:59:59 +0300

    Add project files.

---
+++ ConsoleApp1/AverageClass.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AverageConsoleApp {
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class AverageClass< T > where T : struct {
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        public AverageClass() {
+            // Проверка типа данных 
+            var type = typeof( T );
+            if ( type == typeof( byte )   || type == typeof( sbyte ) || type == typeof( short ) ||
+                 type == typeof( ushort ) || type == typeof( int )   || type == typeof( uint )  ||
+                 type == typeof( long )   || type == typeof( ulong ) || type == typeof( float ) ||
+                 type == typeof( double ) ) { } else {
+                throw new ArgumentException( "Недопустимый тип данных" );
+            }
+        }
+
+        /// <summary>
+        /// Список значений
+        /// </summary>
+        private readonly List< T > _list = new List< T >();
+
+        /// <summary>
+        /// Добавление значения
+        /// </summary>
+        /// <param name="value"></param>
+        public void Add( T value ) => _list.Add( value );
+
+        /// <summary>
+        /// Сброс значений
+        /// </summary>
+        public void Reset() => _list.Clear();
+
+        /// <summary>
+        /// Среднее значение
+        /// </summary>
+        /// <returns></returns>
+        public T Average() {
+            T result = default;
+            foreach ( var item in _list ) {
+                result = Add( result, item );
+            }
+            result = Div( result, _list.Count );
+            return result;
+        }
+
+        /// <summary>
+        /// Сложение
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        private static T Add( T a, T b ) => ( T ) ( ( dynamic ) a + b );
+
+        /// <summary>
+        /// Деление
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        private static T Div( T a, int b ) => ( T ) ( ( dynamic ) a / b );
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using AverageConsoleApp;
+
+namespace ConsoleApp1 {
+    class Program {
+        static void Main( string[] args ) {
+            // Тестирование
+            var test = new AverageClass< byte >();
+            for ( byte i = 0; i < 10; i++ ) {
+                test.Add( i );
+            }
+            var result = test.Average();
+            test.Reset();
+
+            var test_string = new AverageClass< DateTime >();
+
+            Console.ReadLine();
+
+        }
+    }
+}
